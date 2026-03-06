e6e08ea3bfcfddc671c73542855df9a91d752e9f
NOAHSDELLLAPTOP\noaha <noahafriat11@gmail.com>
2026-03-06 16:37:24 -0500

    Bug fix: Building info popup appears in directions search

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -204,16 +204,12 @@ export default function MapScreen({ initialShowSearch = false }) {
     setOriginMode('manual');
     setOriginBuildingId(building.id);
     setOriginQuery(`${building.name} (${building.code})`);
-    setSelectedBuildingId(building.id);
-    setPopupVisible(true);
     Keyboard.dismiss();
   };
 
   const handleSelectDestinationFromSearch = (building) => {
     setDestinationBuildingId(building.id);
     setDestinationQuery(`${building.name} (${building.code})`);
-    setSelectedBuildingId(building.id);
-    setPopupVisible(true);
     Keyboard.dismiss();
   };
 
